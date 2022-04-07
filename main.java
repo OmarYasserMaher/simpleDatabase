71a57d8930f80428af18adbc1ee95f8fcd1564c7
OmarYasserMaher <omar.yasser.maher@gmail.com>
2022-04-07 23:31:21 +0200

    added line reader

--- main.java
+++ main.java
@@ -1,9 +1,20 @@
 /**
  * main
  */
+import java.util.*;
 public class Main {
+    
 
+    
+    public static String read_line(Scanner scanner){
+        System.out.print("db > ");
+        String str= scanner.nextLine();
+        return str; 
+    }
     public static void main(String[] args) {
-        System.out.println("hi");
+        Scanner scanner= new Scanner(System.in);
+        while(true){
+        read_line(scanner);
+        }
     }
 }
